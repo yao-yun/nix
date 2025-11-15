b53707a164f1d5e87fe654cd81efc0b4b92148e9
Yaoyun Zhang <yaoyun_zhang@outlook.com>
2025-11-16 01:52:54 +0800

    flatten the hierachy

--- flake.nix
+++ flake.nix
@@ -1,34 +1,119 @@
 {
-  description = "Yaoyun's test NixOS flake";
+  description = "Home Manager configuration of yaoyun";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # home-manager = {
-    #   url = "github:nix-community/home-manager/master";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    yaoyun = {
-      url = "path:./users/yaoyun/";
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixCats.url = "github:BirdeeHub/nixCats-nvim";
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # for pam-fix on Non-nixOS
+    pam_shim.url = "github:Cu3PO42/pam_shim";
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
+      # to have it up-to-date or simply don't specify the nixpkgs input
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland?ref=v0.51.0";
+    hy3 = {
+      url = "github:outfoxxed/hy3?ref=hl0.51.0";
+      inputs.hyprland.follows = "hyprland";
+    };
+    hyprsplit = {
+      url = "github:shezdy/hyprsplit?ref=v0.51.0";
+      inputs.hyprland.follows = "hyprland";
+    };
+    
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      yaoyun,
-      ...
-    }@inputs:
-    {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
+    inputs@{ nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      username = "yaoyun";
+      homeDirectory = "/home/${username}";
+      homeModule = ./home;
+      specialArgs = { inherit inputs; };
+      homePkgs = nixpkgs.legacyPackages.${system};
+
+      homeModules = [
+        inputs.stylix.homeModules.stylix
+        homeModule
+      ];
+
+      nixosHomeManagerModule = {
+        home-manager = {
+          useGlobalPkgs = false;
+          useUserPackages = true;
+          backupFileExtension = "hmbak";
+          users.${username} = homeModule;
+          extraSpecialArgs = specialArgs;
+        };
+      };
+
+      nixosLocalModules = [
+        ./nixos/modules/steam.nix
+        ./nixos/modules/stylix.nix
+      ];
+
+      nixosUserModule =
+        { pkgs, ... }:
+        {
+          users.users.${username} = {
+            isNormalUser = true;
+            home = homeDirectory;
+            shell = pkgs.fish;
+            extraGroups = [
+              "NetworkManager"
+              "wheel"
+            ];
+            password = "changeme";
+            openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpzFyoXVgbIDrdlyuG0qXAnmMh6TEINBvI/TD98NvNe openpgp:0x99234C74"
+            ];
+          };
+
+          programs.fish.enable = true;
+
+          i18n.extraLocales = [
+            "en_US.UTF-8/UTF-8"
+            "zh_CN.UTF-8/UTF-8"
+          ];
+        };
+      sharedNixosModules =
+        [
+          inputs.stylix.nixosModules.stylix
+          inputs.home-manager.nixosModules.home-manager
+          nixosHomeManagerModule
         ]
-        ++ yaoyun.nixosModules."yaoyun";
-        # use my (user perspective) nixos conf with home-managers
+        ++ nixosLocalModules
+        ++ [ nixosUserModule ];
+    in
+    {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        pkgs = homePkgs;
+        modules = homeModules;
+        extraSpecialArgs = specialArgs;
+      };
+
+      nixosModules = {
+        ${username} = sharedNixosModules;
+        vmGuest = import ./nixos/modules/vm-guest.nix;
       };
     };
 }
--- home/modules/desktop-env.nix
+++ home/modules/desktop-env.nix
@@ -8,8 +8,8 @@
   imports = [
     ./stylix.nix
     ./fcitx.nix
-    ./programs/caelestia
-    ./programs/hyprland
+    ../programs/caelestia
+    ../programs/hyprland
     # ./pam.nix
     inputs.pam_shim.homeModules.default
   ];
